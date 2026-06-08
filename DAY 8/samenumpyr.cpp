23622086e85a1fca16f7fd6e78312e062007fbd4
Glb_25114CN074 <yaksh4063@gmail.com>
2026-06-08 19:47:44 +0530

    Day-8 uploaded.

---
+++ DAY 8/samenumpyr.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int j=0;
+
+    for(int i=1;i<=5;i++)
+    {
+        j++;
+        for(int k=1;k<=i;k++)
+        {
+            cout<<j<<"";
+        }
+    
+    cout<<endl;
+    }
+
+return 0;
+}
