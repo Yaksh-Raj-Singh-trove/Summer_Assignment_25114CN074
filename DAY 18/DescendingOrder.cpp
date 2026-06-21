3e2187c0e0cca49973befb532318ee4217c13c7c
Glb_25114CN074 <yaksh4063@gmail.com>
2026-06-21 20:42:13 +0530

    Day-18 Uploaded.

---
+++ DAY 18/DescendingOrder.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+void sortDescending(int arr[], int n)
+{
+    for(int i = 0; i < n - 1; i++)
+    {
+        for(int j = i + 1; j < n; j++)
+        {
+            if(arr[i] < arr[j])
+            
+            {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter elements:\n";
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    sortDescending(arr, n);
+
+    cout << "Array in Descending Order: ";
+    for(int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
